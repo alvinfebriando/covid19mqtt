e4e3cf4b85bb641186b127732ad578b8b170289d
Alvin Febriando <alvinfebriando@outlook.com>
2020-05-09 10:40:22 +0700

    Extract prompt question to a function

--- src/client.ts
+++ src/client.ts
@@ -8,19 +8,44 @@ import {
 import { Subscriber } from './subscriber';
 import emitter from './event';
 
-const loop = async (mqttClient: Subscriber) => {
-  const fieldAnswer: { field: string } = await prompt({
+const fieldQuestion = async () => {
+  return <{ field: string }>await prompt({
     type: 'select',
     name: 'field',
     message: 'Informasi apa yang anda butuhkan?',
     choices: Object.values(fieldChoices),
   });
-  const scopeAnswer: { scope: string } = await prompt({
+};
+
+const scopeQuestion = async () => {
+  return <{ scope: string }>await prompt({
     type: 'select',
     name: 'scope',
     message: 'Pilih global atau negara',
     choices: Object.values(scopeChoices),
   });
+};
+
+const answerQuestion = async () => {
+  return <{ country: string }>await prompt({
+    type: 'autocomplete',
+    name: 'country',
+    message: 'Pilih satu negara',
+    choices: await getCountriesName(),
+  });
+};
+
+const loopQuestion = async () => {
+  return <{ loop: string }>await prompt({
+    type: 'confirm',
+    name: 'loop',
+    message: 'Jalankan lagi?',
+  });
+};
+
+const loop = async (mqttClient: Subscriber) => {
+  const fieldAnswer = await fieldQuestion();
+  const scopeAnswer = await scopeQuestion();
   if (scopeAnswer.scope === 'Global') {
     Object.entries(fieldChoices).forEach(field => {
       if (field[1] === fieldAnswer.field) {
@@ -28,12 +53,7 @@ const loop = async (mqttClient: Subscriber) => {
       }
     });
   } else if (scopeAnswer.scope === 'Negara') {
-    const countryAnswer: { country: string } = await prompt({
-      type: 'autocomplete',
-      name: 'country',
-      message: 'Pilih satu negara',
-      choices: await getCountriesName(),
-    });
+    const countryAnswer = await answerQuestion();
     const slug = await getCountriesSlug(countryAnswer.country);
     Object.entries(fieldChoices).forEach(field => {
       if (field[1] === fieldAnswer.field) {
@@ -49,11 +69,7 @@ const start = async () => {
     loop(mqttClient);
   });
   emitter.on('done', async () => {
-    const loopAnswer: { loop: string } = await prompt({
-      type: 'confirm',
-      name: 'loop',
-      message: 'Jalankan lagi?',
-    });
+    const loopAnswer = await loopQuestion();
     if (loopAnswer.loop) {
       loop(mqttClient);
     } else {
