b966c4f0773b7f8991f173cdc30acc7ffee0db2b
Alvin Febriando <alvinfebriando@outlook.com>
2020-05-09 10:32:21 +0700

    Log client when connected and disconnected

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,14 @@ const server = net.createServer(aedes.handle);
 
 const PORT = 1883;
 
+aedes.on('client', client => {
+  console.log(`${client.id} connected`);
+});
+
+aedes.on('clientDisconnect', client => {
+  console.log(`${client.id} disconnected`);
+});
+
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
