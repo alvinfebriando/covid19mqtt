131d25d3dbbaf73fff5b14f24891169d35dd0299
Alvin Febriando <alvinfebriando@outlook.com>
2020-05-11 11:11:17 +0700

    Add more option

--- src/data.ts
+++ src/data.ts
@@ -11,8 +11,11 @@ interface Country {
 
 export enum fieldChoices {
   TotalConfirmed = 'Total Infeksi',
+  NewConfirmed = 'Infeksi Terbaru',
   TotalDeaths = 'Total Meninggal',
+  NewDeaths = 'Meninggal Terbaru',
   TotalRecovered = 'Total Sembuh',
+  NewRecovered = 'Sembuh Terbaru',
 }
 
 export enum scopeChoices {
