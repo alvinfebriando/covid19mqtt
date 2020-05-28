0a60c289d48b8c628c26e133ee03945448452f35
Alvin Febriando <alvinfebriando@outlook.com>
2020-05-28 20:45:54 +0700

    Add more field

--- src/choices.ts
+++ src/choices.ts
@@ -1,10 +1,13 @@
 export enum fieldChoices {
   TotalConfirmed = 'Total Infeksi',
   NewConfirmed = 'Infeksi Terbaru',
+  DayOneConfirmed = 'Peningkatan infeksi sejak hari pertama',
   TotalDeaths = 'Total Meninggal',
   NewDeaths = 'Meninggal Terbaru',
+  DayOneDeaths = 'Peningkatan meninggal sejak hari pertama',
   TotalRecovered = 'Total Sembuh',
   NewRecovered = 'Sembuh Terbaru',
+  DayOneRecovered = 'Peningkatan sembuh sejak hari pertama',
 }
 
 export enum scopeChoices {
